122f7e759568e7af67ba306c6ab1a504ad9580ff
GreatEki <ekeneidiagbor@gmail.com>
2020-12-22 16:54:18 +0100

    circle marker feature

--- map1.py
+++ map1.py
@@ -11,7 +11,7 @@ elev = list(data["ELEV"])
 def color_producer(elevation):
     if elevation < 1000:
         return 'green'
-    elif 1000 <= elevation < 3000:
+    elif elevation > 1000 and elevation < 3000:
         return 'orange'
     else:
         return 'red'
@@ -23,7 +23,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 # Adding Multiple Points: To do this we use a for loop
 for lt, ln, el in zip(lat, long, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+ " m", icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ " m", fill_color=color_producer(el), color='grey', fill_opacity=0.7, fill=True))
 
 map.add_child(fg)
 map.save("Map1.html")
