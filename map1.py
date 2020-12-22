b94f3814b1515819c91534ab2d96ec7cb6896b22
GreatEki <ekeneidiagbor@gmail.com>
2020-12-22 16:31:13 +0100

    adding color points to map

--- map1.py
+++ map1.py
@@ -2,22 +2,33 @@ import folium
 import pandas
 import os
 
-if os.path.exists('volcano.txt'):
-    data = pandas.read_csv('volcano.txt')
-    lat = list(data['LAT'])
-    long = list(data['LON'])
-    map = folium.Map(location=[6.598, 3.4141], zoom_start=6)
-    # Adding Points to the Map
-    fg = folium.FeatureGroup(name="My Map")
+# if os.path.exists('volcano.txt'):
+data = pandas.read_csv('volcano.txt')
+lat = list(data['LAT'])
+long = list(data['LON'])
+elev = list(data["ELEV"])
 
-    # Adding Multiple Points: To do this we use a for loop
-    for lt, ln in zip(lat, long):
-        fg.add_child(folium.Marker(location=[lt, ln], popup="I am a Marker", icon=folium.Icon(color='blue')))
-    
-    map.add_child(fg)
-    map.save("Map1.html")
-else:
-    print('Path to file does not exist')
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+map = folium.Map(location=[6.598, 3.4141], zoom_start=6)
+# Adding Points to the Map
+fg = folium.FeatureGroup(name="My Map")
+
+# Adding Multiple Points: To do this we use a for loop
+for lt, ln, el in zip(lat, long, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+ " m", icon=folium.Icon(color=color_producer(el))))
+
+map.add_child(fg)
+map.save("Map1.html")
+# else:
+# print('Path to file does not exist')
 
 
 
