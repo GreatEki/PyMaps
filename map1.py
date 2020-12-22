a8e3a349edb4de46901fd33b22584f3f8a0ff2e1
GreatEki <ekeneidiagbor@gmail.com>
2020-12-22 16:02:32 +0100

    read files for maps

--- map1.py
+++ map1.py
@@ -1,13 +1,24 @@
 import folium
-map = folium.Map(location=[6.598, 3.4141], zoom_start=6)
+import pandas
+import os
 
-# Adding Points to the Map
-fg = folium.FeatureGroup(name="My Map")
+if os.path.exists('files/Volcanoes.txt'):
+    data = pandas.read_csv('files/Volcanoes.txt')
+    lat = list(data['LAT'])
+    long = list(data['LON'])
+    map = folium.Map(location=[6.598, 3.4141], zoom_start=6)
+    # Adding Points to the Map
+    fg = folium.FeatureGroup(name="My Map")
 
-# Adding Multiple Points: To do this we use a for loop
-for coordinates in [[6.578, 3.42], [6.432, 3.389]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="I am a Marker", icon=folium.Icon(color='blue')))
+    # Adding Multiple Points: To do this we use a for loop
+    for lt, ln in zip(lat, long):
+        fg.add_child(folium.Marker(location=[lt, ln], popup="I am a Marker", icon=folium.Icon(color='blue')))
+    
+    map.add_child(fg)
+    map.save("Map1.html")
+else:
+    print('Path to file does not exist')
 
 
-map.add_child(fg)
-map.save("Map1.html")
+
+
